6875319baf2a737dd8fe458327fd5d7023a34e22
Luke Breen <l.breen1@outlook.com>
2016-08-18 15:12:24 +0100

    User can now add a new cocktail

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -8,8 +8,15 @@ class CocktailsController < ApplicationController
   def show
   end
   def new
+    @cocktail = Cocktail.new
   end
   def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   private
