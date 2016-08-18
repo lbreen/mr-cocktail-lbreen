4cc6daee147f5067bb637ec7434a4a007426160b
Luke Breen <l.breen1@outlook.com>
2016-08-18 17:18:51 +0100

    User can delete ingredients

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,29 @@
 class DosesController < ApplicationController
+
+  before_action :find_cocktail, only: [:new, :create]
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail_id = @cocktail.id
+    @dose.save ? (redirect_to cocktail_path(@cocktail)) : (render :new)
+  end
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:cocktail_id, :ingredient_id, :description)
+  end
 end
