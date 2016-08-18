1bc61d4d36c01ec582d1350783acab0917dae33d
Luke Breen <l.breen1@outlook.com>
2016-08-18 11:38:49 +0100

    Created cocktails, ingredients and doses tables - fully seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Cocktail.destroy_all
+Ingredient.destroy_all
+Dose.destroy_all
+
+cocktails = []
+ingredients = %w(vodka whiskey rum ice lemon tonic mint lime vermouth coke)
+ingredient_instances = []
+
+5.times do
+  cocktails << Cocktail.create({
+    name: Faker::Beer.name
+    })
+end
+
+10.times do
+  i = rand(0..9)
+  ingredient_instances << Ingredient.create({
+    name: ingredients[i]
+    })
+end
+
+5.times do
+  i = rand(0..9)
+  c = rand(0..4)
+  Dose.create({
+    cocktail: cocktails[c],
+    ingredient: ingredient_instances[i]
+    })
+end
