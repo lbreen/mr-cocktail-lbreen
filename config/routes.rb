30dc4d23d5198a2168e7039f24ad631f29ee2fa4
Luke Breen <l.breen1@outlook.com>
2016-08-19 13:50:19 +0100

    Photo upload integrated

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount Attachinary::Engine => "/attachinary"
 
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:new, :create, :destroy]
   end
 
+
   root "cocktails#index"
 
-  mount Attachinary::Engine => "/attachinary"
 end
